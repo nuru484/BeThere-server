f8054bf3036fc36dd9c1e83ff3f6699311521166
Abdulmajeed Nurudeen <abdulmajeednurudeen48@gmail.com>
2025-10-28 23:11:51 +0000

    update db schema to handle new update of face auth system scan

---
+++ prisma/migrations/20251028231118_init/migration.sql
@@ -0,0 +1,9 @@
+/*
+  Warnings:
+
+  - The `faceScan` column on the `User` table would be dropped and recreated. This will lead to data loss if there is data in the column.
+
+*/
+-- AlterTable
+ALTER TABLE "User" DROP COLUMN "faceScan",
+ADD COLUMN     "faceScan" JSONB;
