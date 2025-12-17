7778ae6f378f8706d30fb5da1fdc7ac37ae55481
Abdulmajeed Nurudeen <abdulmajeednurudeen48@gmail.com>
2025-12-17 22:29:34 +0000

    update to remove generated

--- src/config/prisma-client.js
+++ src/config/prisma-client.js
@@ -1,6 +1,6 @@
 // src/config/prisma-client.js
 import { PrismaPg } from "@prisma/adapter-pg";
-import { PrismaClient } from "../../generated/prisma/client.js";
+import { PrismaClient } from "@prisma/client";
 
 const connectionString = `${process.env.DATABASE_URL}`;
 
